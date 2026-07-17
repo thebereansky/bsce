aba9b14e89c4ed29c1b8dd376cc4e76c830bb995
The Berean Sky <thebereansky@gmail.com>
2026-07-16 23:40:29 -0400

    Add Track Curriculum Generator script
    This script generates a track curriculum and saves it as a JSON file.

---
+++ src/apps-script/TrackCurriculumGenerator.js
@@ -0,0 +1,38 @@
+/**
+ * Track Curriculum Generator
+ * Version 1.0
+ */
+
+function generateTrackCurriculum() {
+
+  Logger.log(
+    "Generating Track Curriculum..."
+  );
+
+  const prompt =
+    buildTrackCurriculumPrompt();
+
+  const response =
+    generateContent(
+      prompt
+    );
+
+  saveCurrentStudyFile(
+
+    response,
+
+    "track-curriculum-plan.json",
+
+    "05 - Production"
+
+  );
+
+  Logger.log(
+    "Track Curriculum Saved."
+  );
+
+  return JSON.parse(
+    response
+  );
+
+}
