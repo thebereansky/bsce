4376f1ec6c4df3eccd122cff3ffc72e03ef86154
The Berean Sky <thebereansky@gmail.com>
2026-07-17 18:32:07 -0400

    Add TrackDefinitionRepository with getTrackDefinition function
    This file contains the implementation of the Track Definition Repository, including the getTrackDefinition function to retrieve track definitions based on track IDs.

---
+++ src/apps-script/TrackDefinitionRepository.js
@@ -0,0 +1,55 @@
+/**
+ * Track Definition Repository
+ * Version 1.0
+ */
+
+function getTrackDefinition(
+  trackId
+) {
+
+  const study =
+    getActiveStudy();
+
+  const seriesFolder =
+    getSeriesFolder(
+      buildSeriesFolderName(
+        study
+      )
+    );
+
+  const filename =
+
+    "track-" +
+
+    trackId +
+
+    "-definition.json";
+
+  const files =
+    seriesFolder.getFilesByName(
+      filename
+    );
+
+  if (
+    !files.hasNext()
+  ) {
+
+    throw new Error(
+
+      "Track definition not found: " +
+
+      trackId
+
+    );
+
+  }
+
+  return JSON.parse(
+
+    files.next()
+      .getBlob()
+      .getDataAsString()
+
+  );
+
+}
