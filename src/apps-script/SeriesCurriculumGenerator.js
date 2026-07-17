7ce6237e39f8869874b37322117bff8ea103a6a0
The Berean Sky <thebereansky@gmail.com>
2026-07-17 00:03:57 -0400

    Upgrade Series Curriculum Generator to version 2.0

--- src/apps-script/SeriesCurriculumGenerator.js
+++ src/apps-script/SeriesCurriculumGenerator.js
@@ -1,32 +1,51 @@
 /**
  * Series Curriculum Generator
- * Version 1.0
+ * Version 2.0
+ *
+ * Generates the master curriculum
+ * roadmap for all Series.
  */
 
 function generateSeriesCurriculum() {
 
   Logger.log(
-    "Generating Series Curriculum..."
+    "========================================"
+  );
+
+  Logger.log(
+    "GENERATING SERIES CURRICULUM"
+  );
+
+  Logger.log(
+    "========================================"
   );
 
   const prompt =
     buildSeriesCurriculumPrompt();
+
+  Logger.log(
+    "Sending curriculum prompt..."
+  );
 
   const response =
     generateContent(
       prompt
     );
 
+  Logger.log(
+    "Saving series curriculum..."
+  );
+
   saveSeriesRootFile(
-  
+
     response,
-  
+
     "series-curriculum-plan.json"
-  
+
   );
 
   Logger.log(
-    "Series Curriculum Saved."
+    "Series curriculum saved."
   );
 
   return JSON.parse(
