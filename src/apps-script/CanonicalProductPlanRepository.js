7071bd7efd2f1809b867bd54d7d263605ac9d161
The Berean Sky <thebereansky@gmail.com>
2026-07-16 23:34:41 -0400

    Rename function and update version in repository

--- src/apps-script/CanonicalProductPlanRepository.js
+++ src/apps-script/CanonicalProductPlanRepository.js
@@ -1,12 +1,79 @@
 /**
- * Canonical Product Plan Repository
- * Version 1.0
+ * Canonical Product Plan Builder
+ * Version 2.0
  */
 
-function getCanonicalProductPlan() {
+function buildCanonicalProductPlan() {
 
-  return loadGeneratedAsset(
-    "canonical-product-plan"
-  );
+  const analysis =
+    analyzeStudy();
+
+  return {
+
+    version:
+      "1.0",
+
+    generatedOn:
+      new Date()
+        .toISOString(),
+
+    series:
+      analysis.series,
+
+    track:
+      analysis.track,
+
+    study:
+      analysis.study,
+
+    primaryScripture:
+      analysis.primaryScripture,
+
+    bigIdea:
+      analysis.bigIdea,
+
+    themes:
+      analysis.themes,
+
+    audiences:
+      analysis.audiences,
+
+    scriptureReferences:
+      analysis.scriptureReferences,
+
+    bigQuestion:
+      analysis.bigQuestion,
+
+    introduction:
+      analysis.introduction,
+
+    keyTakeaways:
+      analysis.keyTakeaways,
+
+    reflectionQuestions:
+      analysis.reflectionQuestions,
+
+    familyDiscussionQuestions:
+      analysis.familyDiscussionQuestions,
+
+    productPlans: {
+
+      book: {},
+
+      workbook: {},
+
+      familyGuide: {},
+
+      shorts: [],
+
+      coloringBook: [],
+
+      activityBook: [],
+
+      posters: []
+
+    }
+
+  };
 
 }
