f0ac2ee58113c880a6c7c035cd015df2b979f99f
The Berean Sky <thebereansky@gmail.com>
2026-07-16 22:05:43 -0400

    Upgrade Scene Image Generator to version 2.0

--- src/apps-script/SceneImageGenerator.js
+++ src/apps-script/SceneImageGenerator.js
@@ -1,19 +1,51 @@
 /**
  * Scene Image Generator
- * Version 1.0
+ * Version 2.0
  */
 
 function generateSceneImage(
   scene
 ) {
 
+  const prompt =
+    buildGovernedImagePrompt(
+      scene
+    );
+
+  const validation =
+    validateVisualPrompt(
+      prompt
+    );
+
+  if (
+    !validation.passed
+  ) {
+
+    throw new Error(
+
+      "Visual Governance Failure:\n\n" +
+
+      JSON.stringify(
+
+        validation,
+
+        null,
+
+        2
+
+      )
+
+    );
+
+  }
+
   Logger.log(
-    "Generating image for scene " +
+    "Generating governed image for scene " +
     scene.sceneNumber
   );
 
   return generateImage(
-    scene.videoPrompt
+    prompt
   );
 
 }
