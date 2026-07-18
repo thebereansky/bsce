048815a12a88c36e8416852bad0fc7c00731ffa5
The Berean Sky <thebereansky@gmail.com>
2026-07-18 14:16:18 -0400

    Add CoverStyleRepository with cover style details

---
+++ src/apps-script/CoverStyleRepository.js
@@ -0,0 +1,27 @@
+/**
+ * Cover Style Repository
+ * Version 1.0
+ */
+
+function getCoverStyle() {
+
+  return {
+
+    titleStyle:
+      "outline serif",
+
+    lettering:
+      "hollow",
+
+    subtitleStyle:
+      "outline serif",
+
+    borderStyle:
+      "decorative classic",
+
+    cornerStyle:
+      "ornamental"
+
+  };
+
+}
