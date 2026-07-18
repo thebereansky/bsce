c834e0af88820ec3d98c12e657ee785c41d06f50
The Berean Sky <thebereansky@gmail.com>
2026-07-17 22:38:00 -0400

    Add new folder names to StudySubfolderCreator

--- src/apps-script/StudySubfolderCreator.js
+++ src/apps-script/StudySubfolderCreator.js
@@ -9,14 +9,27 @@ function createStudySubfolders(
   studyName
 ) {
 
-  const folders = [
+const folders = [
 
-    "02 - Master Study",
-    "03 - YouTube",
-    "04 - Digital Products",
-    "05 - Production"
+  "02 - Master Study",
 
-  ];
+  "03 - YouTube",
+
+  "04 - Digital Products",
+
+  "05 - Production",
+
+  "05 - Small Group Materials",
+
+  "06 - Posters & Visuals",
+
+  "07 - Children's Materials",
+
+  "08 - Amazon KDP",
+
+  "09 - Marketing"
+
+];
 
   folders.forEach(
     function(folderName) {
