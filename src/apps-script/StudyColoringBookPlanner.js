b006253300a293d4b604cd34343b03f810f94fc5
The Berean Sky <thebereansky@gmail.com>
2026-07-18 00:50:36 -0400

    Refactor Study Coloring Book Planner to version 2.0
    Updated the version of the Study Coloring Book Planner and refactored the build function to create a complete book plan.

--- src/apps-script/StudyColoringBookPlanner.js
+++ src/apps-script/StudyColoringBookPlanner.js
@@ -1,37 +1,133 @@
 /**
  * Study Coloring Book Planner
- * Version 1.0
+ * Version 2.0
+ *
+ * Creates a complete coloring
+ * book plan for a Study.
  */
 
-function buildStudyColoringPages(
+function buildStudyColoringBookPlan() {
+
+  const study =
+    getActiveStudy();
+
+  return {
+
+    productId:
+      "coloring-book-" +
+      study.study.id,
+
+    productType:
+      "coloring-book",
+
+    title:
+      study.study.title,
+
+    subtitle:
+      "A Biblical Creation Coloring Book",
+
+    pageSize:
+      "8.5x11",
+
+    singleSided:
+      true,
+
+    includeBorders:
+      true,
+
+    targetAudience:
+      "Children 5-10",
+
+    generatedOn:
+      new Date()
+        .toISOString(),
+
+    pageCount:
+      10,
+
+    pages:
+
+      buildPrototypeBookPages(
+        study
+      )
+
+  };
+
+}
+
+
+/**
+ * Prototype book structure.
+ *
+ * We will expand this to
+ * 32 pages after review.
+ */
+function buildPrototypeBookPages(
   study
 ) {
 
   return [
 
-    buildColoringPageManifest(
+    {
+      pageNumber: 1,
+      pageType: "COVER"
+    },
 
-      study,
+    {
+      pageNumber: 2,
+      pageType: "TITLE_PAGE"
+    },
 
-      "SCRIPTURE_SCENE"
+    {
+      pageNumber: 3,
+      pageType: "ABOUT_BOOK"
+    },
 
-    ),
+    {
+      pageNumber: 4,
+      pageType: "SCRIPTURE_SCENE",
+      title: "In the Beginning",
+      scripture: "Genesis 1:1"
+    },
 
-    buildColoringPageManifest(
+    {
+      pageNumber: 5,
+      pageType: "SCRIPTURE_SCENE",
+      title: "God Created the Heavens",
+      scripture: "Genesis 1:1"
+    },
 
-      study,
+    {
+      pageNumber: 6,
+      pageType: "SCRIPTURE_SCENE",
+      title: "God Created the Earth",
+      scripture: "Genesis 1:1"
+    },
 
-      "KEY_CONCEPT"
+    {
+      pageNumber: 7,
+      pageType: "SCRIPTURE_PAGE",
+      title: "Creation Declares His Glory",
+      scripture: "Psalm 19:1"
+    },
 
-    ),
+    {
+      pageNumber: 8,
+      pageType: "SCRIPTURE_PAGE",
+      title: "By Faith We Understand",
+      scripture: "Hebrews 11:3"
+    },
 
-    buildColoringPageManifest(
+    {
+      pageNumber: 9,
+      pageType: "REFLECTION_PAGE",
+      title: "Wonder of Creation"
+    },
 
-      study,
-
-      "MEMORY_VERSE"
-
-    )
+    {
+      pageNumber: 10,
+      pageType: "BACK_COVER"
+    }
 
   ];
 
